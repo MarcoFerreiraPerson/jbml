3cde3d7758dd81c39759bf0708cfeef2be7f95bd
MarcoFerreiraPerson <68199617+MarcoFerreiraPerson@users.noreply.github.com>
2024-03-18 14:13:10 -0400

    Create chain.py

---
+++ chain.py
@@ -0,0 +1,64 @@
+import requests
+
+server_url = "http://69.141.168.165:8000"
+endpoint = "/generate/"
+retrieve_endpoint = '/jbml_retrieve/'
+
+class LLM_Chain:
+    def __init__(self, system_prompt) -> None:
+        self.chain = f"<s>[INST]{system_prompt}[/INST]Model answer</s> [INST] Follow-up instruction [/INST]"
+
+    def call(self, prompt):
+        self.chain += f"[INST]{prompt}[/INST]"
+        encoded_prompt = requests.utils.quote(self.chain)
+        response = requests.get(f"{server_url}{endpoint}?prompt={encoded_prompt}")
+        if response.status_code == 200:
+            result = response.json()
+            self.chain += result
+        else:
+            print("Error:", response.status_code, response.text)
+            result = None
+        return result
+    def call_jbml(self, prompt): 
+        context, metadata = get_rag_prompt(prompt)
+        self.chain += f"[INST]{prompt} \nOnly use context from here for your response: \n{context} [End of context][/INST]"
+        encoded_prompt = requests.utils.quote(self.chain)
+        response = requests.get(f"{server_url}{endpoint}?prompt={encoded_prompt}")
+        if response.status_code == 200:
+            result = response.json()
+            self.chain += result
+            
+        else:
+            print("Error:", response.status_code, response.text)
+            result = None
+        return result, context, metadata
+    
+    @DeprecationWarning
+    def stream(self, prompt):
+        self.chain += f"[INST]{prompt}[/INST]"
+        encoded_prompt = requests.utils.quote(self.chain)
+        response = requests.get(f"{server_url}{endpoint}?prompt={encoded_prompt}")
+        if response.status_code == 200:
+            result = response.json()
+            for char in result[0]:
+                self.chain += char
+        else:
+            print("Error:", response.status_code, response.text)
+            result = None
+        yield result
+def get_rag_prompt(prompt):
+    system_prompt = "You are an AI designed to take apart the important part of the prompt for Retrieval Search. Return the phrase or words that are unknown to you or you need more information about."
+    retrieval = f"<s>[INST]{system_prompt}[/INST] Model answer</s> [INST] Follow-up instruction [/INST]"
+    
+    
+    retrieval += f"[INST]{prompt}[/INST]"
+    encoded_prompt = requests.utils.quote(prompt)
+    context_json = requests.get(f"{server_url}{retrieve_endpoint}?prompt={encoded_prompt}")
+    if context_json.status_code == 200:
+        context = context_json.json()[0]
+        metadata = context_json.json()[1]
+    else:
+        print("Error:", context.status_code, context.text)
+        context, metadata = 'An error has occured', {}
+    return context, metadata
+
