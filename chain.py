29745f32b5f0b9ce7ad9fb50054fb58418739238
mkripinski <164096839+mkripinski@users.noreply.github.com>
2024-07-10 10:06:29 -0400

    Update chain.py

--- chain.py
+++ chain.py
@@ -4,7 +4,7 @@ import re
 import time
 from prompts import CHAT, RAG
 
-server_url = "http://jbml.asuscomm.com:8000"
+server_url = "http://jbmdl.asuscomm.com:8000"
 
 #server_url = "https://penguin-true-cow.ngrok-free.app"
 
